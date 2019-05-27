1ceda2ad1873989fef9e036c180fd5168a7933c9
Raymond de Bruine <raymond.debruine@gmail.com>
2019-05-27 16:53:10 +0200

    test working

--- src/test/java/com/spotitube/app/dao/UserDAOTest.java
+++ src/test/java/com/spotitube/app/dao/UserDAOTest.java
@@ -5,7 +5,9 @@ import com.spotitube.app.DTO.UserLoginResponseDTO;
 import com.spotitube.app.dao.src.DatabaseConnection;
 import com.spotitube.app.dao.src.UserDAO;
 import com.spotitube.app.exceptions.NoDatabaseConnectionException;
+import com.spotitube.app.exceptions.NotAuthorizedException;
 import com.spotitube.app.exceptions.UserOrPasswordFailException;
+import com.spotitube.app.exceptions.UserTokenException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,20 +49,90 @@ public class UserDAOTest {
         dto.setPassword(PASSWORD);
 
         Mockito.when(statement.executeQuery()).thenReturn(set);
-        Mockito.when(set.first()).thenReturn(true);
-        Mockito.when(set.getInt(Mockito.anyInt())).thenReturn(1);
+        Mockito.when(set.getInt(1)).thenReturn(1);
 
-        Assertions.assertTrue(userDAO.loginUser(dto));
+        userDAO.loginUser(dto);
     }
 
     @Test
-    public void saveUserTokenTest() throws SQLException {
+    public void loginUserExceptionTest() throws SQLException, UserOrPasswordFailException {
+        UserLoginDTO dto = new UserLoginDTO();
+        dto.setPassword(PASSWORD);
+        dto.setUser(USERNAME);
+
+        Mockito.when(statement.executeQuery()).thenReturn(set);
+        Mockito.when(set.getInt(1)).thenReturn(0);
+
+        UserOrPasswordFailException exception = Assertions.assertThrows(UserOrPasswordFailException.class, () ->
+            userDAO.loginUser(dto));
+        Assertions.assertEquals("No user found on " + dto.getUser(), exception.getMessage());
+    }
+
+    @Test
+    public void loginUserSQLExceptionTest() throws SQLException, UserOrPasswordFailException {
+        UserLoginDTO dto = new UserLoginDTO();
+        dto.setPassword(PASSWORD);
+        dto.setUser(USERNAME);
+
+        Mockito.when(statement.executeQuery()).thenThrow(new SQLException());
+
+        UserOrPasswordFailException exception = Assertions.assertThrows(UserOrPasswordFailException.class, () ->
+            userDAO.loginUser(dto));
+        Assertions.assertEquals("No user found on " + dto.getUser(), exception.getMessage());
+    }
+
+
+    @Test
+    public void saveUserTokenTest() throws SQLException, UserTokenException {
         UserLoginResponseDTO dto = new UserLoginResponseDTO();
         dto.setToken(TOKEN);
         dto.setUser(USERNAME);
 
         Mockito.when(statement.execute()).thenReturn(true);
 
-        Assertions.assertTrue(userDAO.saveUserToken(dto));
+        userDAO.saveUserToken(dto);
+    }
+
+    @Test
+    public void saveUserTokenSQLExceptionTest() throws SQLException {
+        UserLoginResponseDTO dto = new UserLoginResponseDTO();
+        dto.setToken(TOKEN);
+        dto.setUser(USERNAME);
+
+        Mockito.when(statement.execute()).thenThrow(new SQLException());
+        UserTokenException exception = Assertions.assertThrows(UserTokenException.class, () ->
+            userDAO.saveUserToken(dto));
+
+        Assertions.assertEquals("Failed to save user token of user " + dto.getUser(), exception.getMessage());
+    }
+
+    @Test
+    public void isAuthorizedTest() throws NotAuthorizedException, SQLException {
+        Mockito.when(statement.executeQuery()).thenReturn(set);
+        Mockito.when(set.first()).thenReturn(true);
+        Mockito.when(set.getInt(1)).thenReturn(1);
+
+        Assertions.assertTrue(userDAO.isAuthorized(TOKEN));
+    }
+
+    @Test
+    public void isAuthorizedExceptionTest() throws NotAuthorizedException, SQLException {
+        Mockito.when(statement.executeQuery()).thenReturn(set);
+        Mockito.when(set.first()).thenReturn(true);
+        Mockito.when(set.getInt(1)).thenReturn(0);
+
+        NotAuthorizedException exception = Assertions.assertThrows(NotAuthorizedException.class, () ->
+            userDAO.isAuthorized(TOKEN));
+        Assertions.assertEquals("User is not authorized", exception.getMessage());
+    }
+
+
+    @Test
+    public void isAuthorizedSQLExceptionTest() throws NotAuthorizedException, SQLException {
+        Mockito.when(statement.executeQuery()).thenThrow(new SQLException());
+
+        NotAuthorizedException exception = Assertions.assertThrows(NotAuthorizedException.class, () ->
+            userDAO.isAuthorized(TOKEN));
+        Assertions.assertEquals("User is not authorized", exception.getMessage());
     }
 }
